99cba49d924edb54567334c984593b3a68e691f1
hvkcoder <hvkcoder@163.com>
2018-03-21 17:36:03 +0800

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// https://eslint.org/docs/user-guide/configuring
+module.exports = {
+	// specifying root
+	"root": true,
+	// specifying Parser
+	// "parser":"",
+	// specifying Parser Options
+	"parserOptions": {
+		// set EcmaScript Version
+		"ecmaVersion": 6,
+		"sourceType": "module",
+	},
+	// specifying enviroments
+	"env": {
+		// browser global variables
+		"browser": true,
+		// Node.js global variables and Node.js scoping
+		"node": true
+	},
+	// configuring plugins
+	"plugins": [],
+	// configuring rules
+	"rules": {
+		// set "quotes" style is "double" or "single"
+		"quotes": ["error", "double"],
+		// set "semi"
+		"semi": [2, "always"]
+	}
+};
